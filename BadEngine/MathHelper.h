9e13fbf26c73fe5a330e89abb1aca1ecbb5c8d2e
Athos <athosvankralingen@hotmail.com>
2015-10-27 21:55:27 +0100

    Forgot header for pingpong method

--- BadEngine/MathHelper.h
+++ BadEngine/MathHelper.h
@@ -7,7 +7,9 @@ namespace BadEngine
     public:
         MathHelper() = delete;
         ~MathHelper() = delete;
+        MathHelper(const MathHelper& a_MatheHelper) = delete;
 
         static float clamp(float a_Value, float a_Min, float a_Max);
+        static float pingPong(float a_Value, float a_Min, float a_Max);
     };
 }
