c2278a34d450d1782407a842cd91a8f307df12fa
Athos <athosvankralingen@hotmail.com>
2015-10-31 00:21:20 +0100

    Implemented mouse position

--- BadEngine/Mouse.cpp
+++ BadEngine/Mouse.cpp
@@ -10,7 +10,7 @@ Mouse::~Mouse()
 
 glm::vec2 Mouse::getPosition() const
 {
-    return glm::vec2(0, 0);
+    return m_Position;
 }
 
 void Mouse::setPosition(glm::vec2 a_Position)
@@ -24,12 +24,21 @@ void Mouse::process(SDL_MouseButtonEvent a_ButtonEvent)
 
 void Mouse::process(SDL_MouseMotionEvent a_MotionEvent)
 {
+    m_CurrentMotion = a_MotionEvent;
 }
 
 void Mouse::process(SDL_MouseWheelEvent a_ScrollEvent)
 {
+
 }
 
 void Mouse::update()
 {
+    updatePosition();
+}
+
+void Mouse::updatePosition()
+{ 
+    m_Position.x = m_CurrentMotion.x;
+    m_Position.y = m_CurrentMotion.y;
 }
--- BadEngine/Mouse.h
+++ BadEngine/Mouse.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <glm/glm.hpp>
 #include <SDL2.0.3/SDL.h>
+#include <vector>
 
 enum class MouseButton
 {
@@ -14,6 +15,9 @@ class Mouse
 private:
     glm::vec2 m_Position;
     glm::vec2 m_PreviousPosition;
+    SDL_MouseMotionEvent m_CurrentMotion;
+    std::vector<SDL_MouseButtonEvent> m_ButtonEvents;
+    std::vector<SDL_MouseWheelEvent> m_ScrollEvents;
 
 public:
     Mouse();
@@ -26,5 +30,8 @@ public:
     void process(SDL_MouseMotionEvent a_MotionEvent);
     void process(SDL_MouseWheelEvent a_ScrollEvent);
     void update();
+
+private:
+    void updatePosition();
 };
 
