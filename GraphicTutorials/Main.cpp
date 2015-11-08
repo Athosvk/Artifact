241084c0e08a029f9d6b5f9b527877cabd8a725a
Athos <athosvankralingen@hotmail.com>
2015-11-08 20:50:24 +0100

    Removed unused include

--- GraphicTutorials/Main.cpp
+++ GraphicTutorials/Main.cpp
@@ -1,10 +1,8 @@
-#include <BadEngine/Game.h>
-
 #include "MainGame.h"
 
 int main(int argumentCount, char* arguments[]) 
 {
-    MainGame mainGame;        
+    MainGame mainGame;
 
     mainGame.run();
 
