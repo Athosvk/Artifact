0efcf4bb7581ae2b434ecf5bfa85fd8e3ba7c45b
Athos van Kralingen <athosvankralingen@hotmail.com>
2015-12-31 08:22:39 +0100

    added enemy to the game world

--- GraphicTutorials/MainWorld.cpp
+++ GraphicTutorials/MainWorld.cpp
@@ -1,10 +1,11 @@
 #include "MainWorld.h"
-
 #include "Player/PlayerInputSystem.h"
+#include "Enemy/Enemy.h"
 
 MainWorld::MainWorld(const BadEngine::Window& a_Window)
     : World(a_Window),
     m_Player(m_EntitySystem)
 {
     addSystem<PlayerInputSystem>();
+    m_EntitySystem.createEntity<Enemy>();
 }
