49358b60322c357ba375ef6a0b8a1e0c345eafa1
Athos van Kralingen <athosvankralingen@hotmail.com>
2016-01-31 21:01:54 +0100

    Older cleanup regarding uninitialisated body

--- BadEngine/Physics/RigidBody2D.cpp
+++ BadEngine/Physics/RigidBody2D.cpp
@@ -11,19 +11,12 @@ namespace BadEngine
 
     void RigidBody2D::setVelocity(glm::vec2 a_Velocity)
     {
-        if(m_Body)
-        {
-           m_Body->SetLinearVelocity(b2Vec2(a_Velocity.x, a_Velocity.y));
-        }
+        m_Body->SetLinearVelocity(b2Vec2(a_Velocity.x, a_Velocity.y));
     }
 
     glm::vec2 RigidBody2D::getVelocity() const
     {
-        if(m_Body)
-        {
-            return glm::vec2(m_Body->GetLinearVelocity().x, m_Body->GetLinearVelocity().y);
-        }
-        return glm::vec2(0, 0);
+        return glm::vec2(m_Body->GetLinearVelocity().x, m_Body->GetLinearVelocity().y);
     }
      
     void RigidBody2D::updateState()
