1718296ca422d0e2bb98ca08fb85703d67836b72
Athos van Kralingen <athosvankralingen@hotmail.com>
2016-01-31 08:32:03 +0100

    Small inheritance fix

--- GraphicTutorials/Enemy/Enemy.h
+++ GraphicTutorials/Enemy/Enemy.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <BadEngine/Core/GameObject.h>
 
-class Enemy : BadEngine::GameObject
+class Enemy : public BadEngine::GameObject
 {
 public:
     Enemy(unsigned a_ID, BadEngine::EntitySystem& a_EntitySystem);
