e54cb40ba07ed09e869d2398735d1ce61ddc75d5
Athos <athosvankralingen@hotmail.com>
2016-02-21 10:03:03 +0100

    Added health and collider for health to enemy

--- GraphicTutorials/Enemy/Enemy.cpp
+++ GraphicTutorials/Enemy/Enemy.cpp
@@ -2,15 +2,24 @@
 #include <BadEngine/Rendering/SpriteRenderer.h>
 #include <BadEngine/Transform.h>
 #include <BadEngine/Core/EntitySystem.h>
+#include <BadEngine/Physics/BoxCollider2D.h>
 
 #include "Enemy.h"
+#include "../HealthComponent.h"
 
 Enemy::Enemy(unsigned a_ID, BadEngine::EntitySystem& a_EntitySystem)
     : GameObject(a_ID, a_EntitySystem)
 {
     auto renderer = addComponent<BadEngine::SpriteRenderer>();
     renderer->setTexture(BadEngine::ResourceManager::getTexture("Textures/Zombie textures/skeleton-move_0.png"));
-    renderer->Width = 1;
-    renderer->Height = 1;
+    renderer->Width = 1.0f;
+    renderer->Height = 1.0f;
     getComponent<BadEngine::Transform>()->setLocalPosition(glm::vec2(0.4f, 0.0f));
+
+    auto health = addComponent<HealthComponent>();
+    health->CurrentHealth = health->MaxHealth = 5;
+
+    auto collider = addComponent<BadEngine::BoxCollider2D>();
+    collider->enableTriggerState();
+    collider->setDimensions(glm::vec2(1.0f, 1.0f));
 }
