e4f45433ee1fa0489a8226ff480af0cc90bc377e
Athos van Kralingen <athos.van.kralingen@hva.nl>
2016-02-26 10:23:41 +0100

    Implemented message queueing, added begin/end iterators

--- BadEngine/Core/MessageQueue.cpp
+++ BadEngine/Core/MessageQueue.cpp
@@ -2,13 +2,15 @@
 
 namespace BadEngine
 {
-    MessageQueue::QueuedMessage::QueuedMessage(std::unique_ptr<BadEngine::Message>&& a_Message, std::type_index a_Type)
+    MessageQueue::QueuedMessage::QueuedMessage(std::unique_ptr<BadEngine::Message>&& a_Message, 
+        std::type_index a_Type, GameObject a_Target)
         : m_Message(std::move(a_Message)),
-        m_MessageType(a_Type)
+        m_MessageType(a_Type),
+        m_Target(a_Target)
     {
     }
 
-    Message* MessageQueue::QueuedMessage::getMessage() const
+    const Message* MessageQueue::QueuedMessage::getMessage() const
     {
         return m_Message.get();
     }
@@ -18,7 +20,24 @@ namespace BadEngine
         return m_MessageType;
     }
 
+    GameObject MessageQueue::QueuedMessage::getTarget() const
+    {
+        return m_Target;
+    }
+
     MessageQueue::MessageQueue()
     {
+    }
+
+    //No trailing return type; the compiler will spit out errors if done so
+    std::vector<MessageQueue::QueuedMessage>::iterator MessageQueue::begin()
+    {
+        return m_Messages.begin();
+    }
+
+    //No trailing return type; the compile will spit out errors if done so
+    std::vector<MessageQueue::QueuedMessage>::iterator MessageQueue::end()
+    {
+        return m_Messages.end();
     }
 }
--- BadEngine/Core/MessageQueue.h
+++ BadEngine/Core/MessageQueue.h
@@ -15,24 +15,30 @@ namespace BadEngine
         private:
             std::unique_ptr<Message> m_Message;
             std::type_index m_MessageType;
+            GameObject m_Target;
 
         public:
-            QueuedMessage(std::unique_ptr<Message>&& a_Message, std::type_index a_Type);
+            QueuedMessage(std::unique_ptr<Message>&& a_Message, std::type_index a_Type, GameObject a_Target);
 
-            Message* getMessage() const;
+            const Message* getMessage() const;
             std::type_index getMessageType() const;
+            GameObject getTarget() const;
         };
 
         std::vector<QueuedMessage> m_Messages;
+        std::vector<int> m_Stuff;
 
     public:
         MessageQueue();
 
+        auto begin()->decltype(m_Messages.begin());
+        auto end()->decltype(m_Messages.end());
+
         template<typename TMessageType, typename ...TArguments>
-        void enqueue(TArguments&&... a_MessageArguments)
+        void enqueue(GameObject a_Target, TArguments&&... a_MessageArguments)
         {
-            m_Messages.emplace(std::make_unique<TMessageType>(std::forward<TArguments>(a_MessageArguments)...),
-                typeid(TMessageType));
+            m_Messages.emplace(std::make_unique(std::forward<TArguments>(a_MessageArguments)...),
+                typeid(TMessageType), a_Target);
         }
     };
 }
