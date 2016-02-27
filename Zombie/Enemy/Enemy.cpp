4ffddf18359fbd61884d20bca4a96f19b7024f0b
Athos <athosvankralingen@hotmail.com>
2016-02-27 15:05:36 +0100

    Added follow component to zombies

--- Zombie/Enemy/Enemy.cpp
+++ Zombie/Enemy/Enemy.cpp
@@ -3,9 +3,11 @@
 #include <BadEngine/Transform.h>
 #include <BadEngine/Core/EntitySystem.h>
 #include <BadEngine/Physics/BoxCollider2D.h>
+#include <BadEngine/Physics/RigidBody2D.h>
 
 #include "Enemy.h"
 #include "../HealthComponent.h"
+#include "FollowComponent.h"
 
 Enemy::Enemy(unsigned a_ID, BadEngine::EntitySystem& a_EntitySystem)
     : GameObject(a_ID, a_EntitySystem)
@@ -14,7 +16,6 @@ Enemy::Enemy(unsigned a_ID, BadEngine::EntitySystem& a_EntitySystem)
     renderer->setTexture(BadEngine::ResourceManager::getTexture("Textures/Zombie textures/skeleton-move_0.png"));
     renderer->Width = 1.0f;
     renderer->Height = 1.0f;
-    getComponent<BadEngine::Transform>()->setLocalPosition(glm::vec2(0.4f, 0.0f));
 
     auto health = addComponent<HealthComponent>();
     health->CurrentHealth = health->MaxHealth = 5;
@@ -22,4 +23,10 @@ Enemy::Enemy(unsigned a_ID, BadEngine::EntitySystem& a_EntitySystem)
     auto collider = addComponent<BadEngine::BoxCollider2D>();
     collider->enableTriggerState();
     collider->setDimensions(glm::vec2(1.0f, 1.0f));
+
+    auto followComponent = addComponent<FollowComponent>();
+    followComponent->FollowSpeed = 0.5f;
+
+    auto rigidBody = addComponent<BadEngine::RigidBody2D>();
+    rigidBody->setGravityScale(0.0f);
 }
