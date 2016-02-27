0b2dd38ccca068a3ed868e0ffcd5227cf994d041
Athos <athosvankralingen@hotmail.com>
2016-02-27 15:04:03 +0100

    Added a player component

---
+++ Zombie/Player/PlayerComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <BadEngine/Core/Component.h>
+
+class PlayerComponent : public BadEngine::Component
+{
+public:
+    PlayerComponent(BadEngine::GameObject a_GameObject);
+};
+
