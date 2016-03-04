f03ef125333d05e7bfa00eb1b87331f92d25025f
Athos van Kralingen <athos.van.kralingen@hva.nl>
2016-03-04 23:27:10 +0100

    Fixed namespace inconsistency

--- Artifact/Delegate.h
+++ Artifact/Delegate.h
@@ -3,32 +3,34 @@
 #include <functional>
 #include <utility>
 
-template<typename TFunctionType>
-class Delegate
+namespace Artifact
 {
-private:
-    std::vector<std::function<TFunctionType>> m_Subscribers;
-
-public:
-    template<typename ...TArguments>
-    void operator()(TArguments&&... a_Arguments)
+    template<typename TFunctionType>
+    class Delegate
     {
-        for(auto& subscriber : m_Subscribers)
+    private:
+        std::vector<std::function<TFunctionType>> m_Subscribers;
+
+    public:
+        template<typename ...TArguments>
+        void operator()(TArguments&&... a_Arguments)
         {
-            subscriber(std::forward<TArguments>(a_Arguments)...);
+            for(auto& subscriber : m_Subscribers)
+            {
+                subscriber(std::forward<TArguments>(a_Arguments)...);
+            }
         }
-    }
 
-    Delegate& operator+=(std::function<TFunctionType>& a_Function)
-    {
-        m_Subscribers.push_back(a_Function);
-        return *this;
-    }
+        Delegate& operator+=(std::function<TFunctionType>& a_Function)
+        {
+            m_Subscribers.push_back(a_Function);
+            return *this;
+        }
 
-    Delegate& operator+=(std::function<TFunctionType>&& a_Function)
-    {
-        m_Subscribers.push_back(std::move(a_Function));
-        return *this;
-    }
-};
-
+        Delegate& operator+=(std::function<TFunctionType>&& a_Function)
+        {
+            m_Subscribers.push_back(std::move(a_Function));
+            return *this;
+        }
+    };
+}
