78f52aa00e6031b01e4d04ac973cbd07e483d289
Athos van Kralingen <athos.van.kralingen@hva.nl>
2016-03-05 02:19:21 +0100

    Removed unused stuff

--- Zombie/MainGame.cpp
+++ Zombie/MainGame.cpp
@@ -6,8 +6,5 @@
 
 MainGame::MainGame() : Game(1024, 768, 0, "GameEngine")
 {
-    m_Window.setBackgroundColor(Artifact::Color(30, 30, 30, 255));
-    printf(" *** OpenGL version: %s *** \n You need at least version %5.2f to run the game. \n", 
-           reinterpret_cast<const char*>(glGetString(GL_VERSION)), m_OpenGLVersion);
     loadWorld<StartScreen>();
 }
