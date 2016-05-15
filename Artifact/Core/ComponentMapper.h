bccfafa5e0cdb79ae0948443d0dab2871eb78a8f
Athos <athosvankralingen@hotmail.com>
2016-05-15 23:48:16 +0200

    Added adding and getting of components, so entities and systems can use that to do so

--- Artifact/Core/ComponentMapper.h
+++ Artifact/Core/ComponentMapper.h
@@ -4,6 +4,8 @@
 #include <memory>
 
 #include "ComponentMap.h"
+#include "ComponentHandle.h"
+#include "GameObject.h"
 
 namespace Artifact
 {
@@ -11,5 +13,21 @@ namespace Artifact
 	{
 	private:
 		std::unordered_map<std::type_index, std::unique_ptr<BaseComponentMap>> m_Components;
+		
+	public:
+		template<typename TComponentType>
+		ComponentHandle<TComponentType> addComponent(GameObject a_GameObject)
+		{
+			auto componentMap = static_cast<ComponentMap<TComponentType>*>
+				(m_Components[typeid(TComponentType)].get());
+			return ComponentHandle<TComponentType>(componentMap->addComponent(a_GameObject), 
+				*componentMap);
+		}
+
+		template<typename TComponentType>
+		ComponentMap<TComponentType>& getComponentsOfType()
+		{
+			return m_Components[typeid(TComponentType)];
+		}
 	};
 }
