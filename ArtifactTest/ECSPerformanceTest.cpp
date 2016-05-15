0056ee27c8b0d5c444b80949152e5e3644d3aedd
Athos <athosvankralingen@hotmail.com>
2016-05-15 19:59:15 +0200

    Added test project

---
+++ ArtifactTest/ECSPerformanceTest.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+
+#include "CppUnitTest.h"
+#include "TestUtility.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ArtifactTest
+{		
+	TEST_CLASS(ECSPerformanceTest)
+	{
+		Artifact::MessagingSystem m_MessagingSystem;
+		Artifact::EntitySystem m_EntitySystem = (m_MessagingSystem);
+
+	public:
+		ECSPerformanceTest()
+		{
+			for(int i = 0; i < 1000000; i++)
+			{
+				auto entity = m_EntitySystem.createEntity();
+				entity.addComponent<Artifact::SpriteRenderer>();
+			}
+		}
+
+		TEST_METHOD(TestIteration)
+		{
+			auto timeTaken = TestUtility::measureNS([this]
+			{
+				performIteration();
+			});
+
+			std::string output = "Iteration test time taken: " + 
+				std::to_string(timeTaken / 1e6) + " ms\n";
+			Logger::WriteMessage(output.c_str());
+			//Assert::IsTrue(timeTaken <= 5e6);
+		}
+
+		void performIteration()
+		{
+			for(auto component : m_EntitySystem.getComponentsOfType<Artifact::Transform>())
+			{
+				component->setPosition(glm::vec2(0.0f, 0.0f));
+			}
+		}
+	};
+}
---
+++ ArtifactTest/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: reference additional headers your program requires here
+#include <Artifact\Transform.h>
+#include <Artifact/Core/GameObject.h>
+#include <Artifact/Core/EntitySystem.h>
+#include <Artifact/Core/Component.h>
+#include <Artifact/Core/MessageQueue.h>
+#include <Artifact/Core/MessagingSystem.h>
+#include <Artifact/Core/System.h>
+#include <Artifact/Core/World.h>
+#include <Artifact/Rendering/SpriteRenderer.h>
